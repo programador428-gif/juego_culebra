1e15974e8c42920f7ebdb031731136798ea201dc
Dilan Solorzano <230013094+programador428-gif@users.noreply.github.com>
2026-05-07 21:13:52 -0500

    feat: cuadriculas en canvas

--- serpiente.js
+++ serpiente.js
@@ -1,12 +1,33 @@
 const canvas = document.getElementById("canvasJuego");
 const ctx = canvas.getContext("2d");
+const TAMANIO_CELDA = 25;
 
 function limpiarCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+function dibujarTablero() {
+  ctx.strokeStyle = "#d84ff3";
+  ctx.lineWidth = 1;
+
+  for (let x = 0; x <= canvas.width; x += TAMANIO_CELDA) {
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, canvas.height);
+    ctx.stroke();
+  }
+
+  for (let y = 0; y <= canvas.height; y += TAMANIO_CELDA) {
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(canvas.width, y);
+    ctx.stroke();
+  }
+}
+
 function dibujarTodo() {
   limpiarCanvas();
+  dibujarTablero();
 }
 
 dibujarTodo();
